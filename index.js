493fd0fccd0bbf09e13f8fb30df9103717a06a63
Victoria Gardella <vgardella@teknos.local>
2024-02-16 15:38:06 -0300

    add access to routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('api/', routes);
 
 app.listen(PORT, () => {
     console.log('Escuchando puerto:', PORT);
