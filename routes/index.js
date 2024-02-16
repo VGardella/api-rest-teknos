8737ccd79dc4e10313dd2ee46cf5f3a496f89770
Victoria Gardella <vgardella@teknos.local>
2024-02-16 15:36:46 -0300

    add request route: get folders

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const getFolders = require('../controllers/folders');
+
+// Definimos las rutas y tareas:
+
+router.get('/folders', getFolders);
