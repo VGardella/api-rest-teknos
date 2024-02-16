a31900547eadb55f78c4913dd35c44adbf0c01b8
Victoria Gardella <vgardella@teknos.local>
2024-02-16 14:51:47 -0300

    fix conditional

--- controllers/middlewares.js
+++ controllers/middlewares.js
@@ -2,7 +2,11 @@
 
 const userAuth = (req, res, next) => {
     if (req.params.user !== 'teknos') {
-        res.status(404).send('Usuario incorrecto');
+        return res.status(404).send('Usuario incorrecto');
     }
     next();
 };
+
+/* El return se usa para que, en caso de que no se
+cumpla con el condicional, no se corra el resto del
+programa. Sino salta un código de error */
