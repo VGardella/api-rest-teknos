22b7cdd7084b5c7147af7d68d42a82548206ea8f
Victoria Gardella <vgardella@teknos.local>
2024-02-16 16:02:00 -0300

    add user parameter

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('api/', routes);
+app.use('api/:user/', routes);
 
 app.listen(PORT, () => {
     console.log('Escuchando puerto:', PORT);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const userAuth = require('../controllers/middlewares');
 const getFolders = require('../controllers/folders');
 
 // Definimos las rutas y tareas:
 
-router.get('/folders', getFolders);
+router.get('/folders', userAuth, getFolders);
 
 module.exports = router;
