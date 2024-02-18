c650389d6594cefc46d382842a57d02880a00e0d
Victoria Gardella <vgardella@teknos.local>
2024-02-18 15:39:28 -0300

    add conection to database

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes/index');
+const userAuth = require('./controllers/middlewares');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/local')
+    .then(() => {
+        console.log('Conexion exitosa a la base de datos');
+    })
+    .catch((err) => {
+        console.log('Error en la base de datos:', err);
+    })
 
 app.use(express.json());
-app.use('api/:user/', routes);
+app.use('/api/:user/', userAuth, routes);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Escuchando puerto:', PORT);
 })
