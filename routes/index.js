0afeacc0e59d0204542a9304fd531be3ef4cefdb
Victoria Gardella <vgardella@teknos.local>
2024-02-18 18:45:07 -0300

    add route (message list)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const getFolders = require('../controllers/folders');
+const getMessages = require('../controllers/messages');
 
 // Definimos las rutas y tareas:
 
 router.get('/folders', getFolders);
+router.get('/messages', getMessages);
 
 module.exports = router;
