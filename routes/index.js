cfc11762a573875678dcf9e8e3326976cb555bc9
Victoria Gardella <vgardella@teknos.local>
2024-02-19 10:34:08 -0300

    add route (posting)

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const getFolders = require('../controllers/folders');
 const getMessages = require('../controllers/messages');
+const postMessage = require('../controllers/post');
 
 // Definimos las rutas y tareas:
 
 router.get('/folders', getFolders);
 router.get('/messages', getMessages);
+router.post('/messages', postMessage);
 
 module.exports = router;
