36e2aad30e80705ec8cf5369410a5daef6cc5727
Victoria Gardella <vgardella@teknos.local>
2024-02-20 18:02:06 -0300

    add routes and functionality (pagination)

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,4 @@
-const { Message, fromSchema } = require('../models/messages.model.js');
-
+const Message = require('../models/messages.model.js').Message;
 
 const getMessages = async (req, res) => {
     let filters = req.query;
@@ -35,7 +34,17 @@ const getMessages = async (req, res) => {
     }
 
     try {
-        const list = await Message.find(query);
+        const list = await Message.aggregate([
+            { $match: query },
+            { $lookup : {
+                    "from" : "challenge_folders",
+                    "localField" : "folder",
+                    "foreignField" : "name",
+                    "as" : "folderInfo"
+                }
+            }
+        ]);
+
         if (list.length === 0) {
             return res.status(200).send('No se encontraron mensajes.')
         }
@@ -103,4 +112,87 @@ const updateMessage = async (req, res) => {
 }
 
 
-module.exports = { getMessages, postMessage, deleteMessage, updateMessage };
+const countMail = async (req, res) => {
+    try {
+        const mails = await Message.aggregate([{
+            $group: {
+                "_id": "$from.name",
+                "total_emails" : { $count: {} }
+            }
+        }])
+        return res.status(200).send(mails);
+    } catch (err) {
+        return res.status(500).send('No se pudo obtener lista de correos.')
+    }
+}
+
+
+const paginateMail = async (req, res) => {
+    const { page, limit } = req.params;
+    const mails = (parseInt(page)-1)*parseInt(limit);
+
+    let filters = req.query;
+    let query = { $and: [] };
+
+    if (filters.from !== undefined) {
+        query.$and.push({ 
+            $or: [
+                { "from.name": { $regex: `${filters.from}`, $options: "i"}},
+                { "from.avatar": { $regex: `${filters.from}`, $options: "i"}},
+                { "from.email": { $regex: `${filters.from}`, $options: "i"}},
+            ]
+        })
+    };
+
+    if (filters.to !== undefined) {
+        query.$and.push({
+            $or: [
+                { "to.name": { $regex: `${filters.to}`, $options: "i"}},
+                { "to.email": { $regex: `${filters.to}`, $options: "i"}}
+            ]
+        })
+    };
+
+    if (filters.subject !== undefined) {
+        query.$and.push({
+            "subject": { $regex: `${filters.subject}`, $options: "i"}
+        })
+    };
+
+    if (Object.keys(filters).length === 0) {
+        delete query.$and;
+    }
+
+    try {
+        const mailPage = await Message.aggregate([
+            { $match: query },
+            { $lookup : {
+                    "from" : "challenge_folders",
+                    "localField" : "folder",
+                    "foreignField" : "name",
+                    "as" : "folderInfo"
+                }
+            },
+            { $skip: mails },
+            { $limit: parseInt(limit) }
+        ])
+
+        const count = await Message.aggregate([
+            { $match: query },
+            { $count: "count" }
+        ])
+
+        const mailList = {
+            count: count[0].count,
+            page: parseInt(page),
+            data: mailPage
+        };
+
+        return res.status(200).json(mailList);
+    } catch (err) {
+        return res.status(500).send('Error al cargar los mensajes.')
+    }
+}
+
+
+module.exports = { getMessages, postMessage, deleteMessage, updateMessage, countMail, paginateMail };
--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const messagesRouter = express.Router();
-const { getMessages, postMessage, deleteMessage, updateMessage } = require('../controllers/messages.controller');
+const { getMessages, postMessage, deleteMessage, updateMessage, countMail, paginateMail } = require('../controllers/messages.controller');
 
 // Definimos las rutas y tareas:
 
@@ -8,5 +8,7 @@ messagesRouter.get('/', getMessages);
 messagesRouter.post('/', postMessage);
 messagesRouter.delete('/:id', deleteMessage);
 messagesRouter.put('/:id', updateMessage);
+messagesRouter.get('/count', countMail);
+messagesRouter.get('/:page/:limit', paginateMail);
 
 module.exports = messagesRouter;
